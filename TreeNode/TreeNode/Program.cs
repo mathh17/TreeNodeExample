ba028aca54962e384a36d04dfa1e86e5be4851bd
Unknown <mathh17@student.sdu.dk>
2018-03-06 10:31:23 +0100

    Upload to GitHub

---
+++ TreeNode/TreeNode/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeNodeTest
+{
+    public class Tree<T>
+    {
+        private TreeNode<T> root;
+
+        public Tree(T value)
+        {
+            if (value != null)
+            {
+                this.root = new TreeNode<T>(value);
+            }
+        }
+    }
+
+    public class TreeNode<T>
+    {
+        private T value;
+        private bool hasParent;
+        private List<TreeNode<>> children;
+
+        public TreeNode(T value)
+        {
+            if (value != null)
+            {
+                this.value = value;
+                this.children = new list<TreeNode<>>();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
